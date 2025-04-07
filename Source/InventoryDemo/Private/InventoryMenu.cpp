25f9aafbf5d563ff322dadef088a09f553f6b0df
Vicky V <alteneria127@gmail.com>
2025-04-06 23:04:42 -0400

    Inventory is loaded based on pickups in the scene; a few WIP scripts for the pickup system

--- Source/InventoryDemo/Private/InventoryMenu.cpp
+++ Source/InventoryDemo/Private/InventoryMenu.cpp
@@ -2,3 +2,28 @@
 
 
 #include "InventoryMenu.h"
+#include "Components/UniformGridPanel.h"
+
+void UInventoryMenu::Populate(TArray<FInventoryItemData> items)
+{
+	int gridIndex = 0;
+
+	for (auto& Item : items)
+	{
+		SpawnItemCell(Item, gridIndex);
+
+		gridIndex++;
+	}
+}
+
+void UInventoryMenu::AddSingleItem(FInventoryItemData item)
+{
+	int lastItemIndex = GridView_Items->GetChildrenCount();
+	SpawnItemCell(item, lastItemIndex++);
+}
+
+void UInventoryMenu::SpawnItemCell(FInventoryItemData data, int itemIndex)
+{
+	UUserWidget* item = CreateWidget(GetOwningPlayer(), ItemWidget, FName(data.Name + "__Item"));
+	GridView_Items->AddChildToUniformGrid(item, itemIndex / NUMBER_OF_COLUMNS, itemIndex % NUMBER_OF_COLUMNS);
+}
--- Source/InventoryDemo/Public/InventoryMenu.h
+++ Source/InventoryDemo/Public/InventoryMenu.h
@@ -6,6 +6,20 @@
 #include "CommonActivatableWidget.h"
 #include "InventoryMenu.generated.h"
 
+class UUniformGridPanel;
+
+USTRUCT(BlueprintType)
+struct FInventoryItemData
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	FString Name;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	FString Description;
+};
+
 /**
  * 
  */
@@ -13,4 +27,24 @@ UCLASS()
 class INVENTORYDEMO_API UInventoryMenu : public UCommonActivatableWidget
 {
 	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditDefaultsOnly,BlueprintReadWrite)
+	TSubclassOf<UCommonActivatableWidget> ItemWidget;
+
+	UFUNCTION(BlueprintCallable)
+	void Populate(TArray<FInventoryItemData> items);
+
+	UFUNCTION(BlueprintCallable)
+	void AddSingleItem(FInventoryItemData item);
+
+private:
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget, BlueprintProtected = true, AllowPrivateAccess = true))
+	TObjectPtr<UUniformGridPanel> GridView_Items;
+
+	//TODO: make it customizable in Editor or have it calculate automatically based on the width of the panel
+	const int16 NUMBER_OF_COLUMNS = 6;
+
+	void SpawnItemCell(FInventoryItemData data, int itemIndex);
 };
